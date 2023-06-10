a643f018cb8197b9e1fda44ff9ee1f532868d731
NovaFugaz <nacchi@outlook.com>
2023-06-10 15:10:42 -0400

    Commit Inicial!

---
+++ JsonToSRT.py
@@ -0,0 +1,61 @@
+import json
+
+def convert_to_srt(json_data):
+    """
+    La función toma el contenido del JSON que le usuarie da y hace un for para cada línea y le da formato, almacenando en memoria el contenido en srt_content.
+    """
+    lines = json_data["lyrics"]["lines"]
+    srt_content = ""
+
+    for i in range(len(lines)):
+        line = lines[i]
+        start_time = int(line["startTimeMs"]) // 1000
+        if i+1 < len(lines):
+            next_line = lines[i+1]
+            end_time = int(next_line["startTimeMs"]) // 1000
+        else:
+            # Última línea, establecer end_time en 0
+            end_time = 0
+        text = line["words"]
+
+        srt_content += f"{i+1}\n"
+        srt_content += f"{format_time(start_time)} --> {format_time(end_time)}\n"
+        srt_content += f"{text}\n"
+        srt_content += "\n"
+
+    return srt_content
+
+
+def format_time(milliseconds):
+    """
+    La función convierte los milisegundos en horas, minutos, segundos y milisegundos según corresponde y devuelve siguiendo el formato hh:mm:ss:msms.
+    """
+    hours = milliseconds // 3600
+    minutes = (milliseconds // 60) % 60
+    seconds = milliseconds % 60
+    milliseconds = (milliseconds % 1000) // 10
+
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:02d}"
+
+# Solicitar al usuario ingresar el JSON
+json_data = input("Ingrese el contenido JSON: ")
+
+# Parsear el JSON ingresado
+try:
+    json_data = json.loads(json_data)
+except ValueError:
+    print("Error: El JSON ingresado no es válido.")
+    exit()
+
+# Solicitar al usuario ingresar el nombre del archivo SRT (sin la extensión)
+srt_filename = input("Ingrese el nombre del archivo SRT para guardar (sin la extensión .srt): ")
+srt_filename += ".srt"  # Agregar la extensión .srt
+
+# Convierte a formato SRT
+srt_data = convert_to_srt(json_data)
+
+# Guarda el archivo SRT
+with open(srt_filename, "w", encoding='utf-8') as file:
+    file.write(srt_data)
+
+print("El archivo SRT ha sido generado exitosamente.")
